dc2f97efa432598965cc9470474ced31ae1bf767
Demianiuk Svitlana <demianiuksvitlana@Mac.docsis.vodafone.cz>
2025-11-19 02:01:57 +0100

    Fix 02_salary_growth to use value filtered by data_type

--- sql_scripts/02_salary_growth.sql
+++ sql_scripts/02_salary_growth.sql
@@ -1,13 +1,13 @@
 -- Dotaz 2: Růst mezd v procentech (pouze primární tabulka)
 WITH avg_salary AS (
-    SELECT year, ROUND(AVG(avg_salary)::numeric,2) AS avg_salary
-    FROM t_svitlana_demianiuk_project_SQL_primary_final
+    SELECT year, ROUND(AVG(value) FILTER (WHERE data_type='salary')::numeric,2) AS avg_salary
+    FROM t_svitlana_demianiuk_project_sql_primary_final
     GROUP BY year
 )
 SELECT
     year,
     LAG(avg_salary) OVER (ORDER BY year) AS prev_salary,
     avg_salary,
-    ROUND(((avg_salary - LAG(avg_salary) OVER (ORDER BY year)) / NULLIF(LAG(avg_salary) OVER (ORDER BY year),0) * 100)::numeric, 2) AS growth_pct
+    ROUND(((avg_salary - LAG(avg_salary) OVER (ORDER BY year)) / NULLIF(LAG(avg_salary) OVER (ORDER BY year),0)) * 100::numeric, 2) AS growth_pct
 FROM avg_salary
 ORDER BY year;
