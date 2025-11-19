a94c4b6fcad822bf6ed3e5f095c77259659a9150
Demianiuk Svitlana <demianiuksvitlana@Mac.docsis.vodafone.cz>
2025-11-19 02:02:22 +0100

    Fix 04_price_growth to use value filtered by data_type

--- sql_scripts/04_price_growth.sql
+++ sql_scripts/04_price_growth.sql
@@ -1,13 +1,13 @@
 -- Dotaz 4: Růst cen v procentech (pouze primární tabulka)
 WITH avg_price AS (
-    SELECT year, ROUND(AVG(avg_price)::numeric,2) AS avg_price
-    FROM t_svitlana_demianiuk_project_SQL_primary_final
+    SELECT year, ROUND(AVG(value) FILTER (WHERE data_type='price')::numeric,2) AS avg_price
+    FROM t_svitlana_demianiuk_project_sql_primary_final
     GROUP BY year
 )
 SELECT
     year,
     LAG(avg_price) OVER (ORDER BY year) AS prev_price,
     avg_price,
-    ROUND(((avg_price - LAG(avg_price) OVER (ORDER BY year)) / NULLIF(LAG(avg_price) OVER (ORDER BY year),0) * 100)::numeric, 2) AS price_growth_pct
+    ROUND(((avg_price - LAG(avg_price) OVER (ORDER BY year)) / NULLIF(LAG(avg_price) OVER (ORDER BY year),0)) * 100::numeric, 2) AS price_growth_pct
 FROM avg_price
 ORDER BY year;
