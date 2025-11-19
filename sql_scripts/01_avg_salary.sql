ce4f4e74a563a6a2373a243367b25b53cd875bd1
Demianiuk Svitlana <demianiuksvitlana@Mac.docsis.vodafone.cz>
2025-11-19 02:01:36 +0100

    Fix 01_avg_salary to use value filtered by data_type

--- sql_scripts/01_avg_salary.sql
+++ sql_scripts/01_avg_salary.sql
@@ -1,7 +1,7 @@
 -- Dotaz 1: Průměrná mzda podle let (pouze primární tabulka)
 SELECT
     year,
-    ROUND(AVG(avg_salary)::numeric, 2) AS avg_salary
-FROM t_svitlana_demianiuk_project_SQL_primary_final
+    ROUND(AVG(value) FILTER (WHERE data_type = 'salary')::numeric, 2) AS avg_salary
+FROM t_svitlana_demianiuk_project_sql_primary_final
 GROUP BY year
 ORDER BY year;
