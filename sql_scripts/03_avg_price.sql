66b4eddaab59f2e8ec2ac0679cc030e9e9667e6c
Demianiuk Svitlana <demianiuksvitlana@Mac.docsis.vodafone.cz>
2025-11-19 02:02:09 +0100

    Fix 03_avg_price to use value filtered by data_type

--- sql_scripts/03_avg_price.sql
+++ sql_scripts/03_avg_price.sql
@@ -1,7 +1,7 @@
 -- Dotaz 3: Průměrné ceny podle let (pouze primární tabulka)
 SELECT
     year,
-    ROUND(AVG(avg_price)::numeric, 2) AS avg_price
-FROM t_svitlana_demianiuk_project_SQL_primary_final
+    ROUND(AVG(value) FILTER (WHERE data_type='price')::numeric, 2) AS avg_price
+FROM t_svitlana_demianiuk_project_sql_primary_final
 GROUP BY year
 ORDER BY year;
